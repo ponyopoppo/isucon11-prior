61112c758d07703971a0d2041277dea92cd19d36
MATSUU Takuto <matsuu@gmail.com>
2021-06-21 21:13:22 +0900

    Set os and arch to support non-amd64

--- infra/instance/cookbooks/golang/default.rb
+++ infra/instance/cookbooks/golang/default.rb
@@ -2,7 +2,7 @@ include_cookbook 'xbuild'
 
 version = '1.16.5'
 
-execute "rm -rf /home/isucon/local/golang; /opt/xbuild/go-install #{version} /home/isucon/local/golang" do
+execute "rm -rf /home/isucon/local/golang; /opt/xbuild/go-install #{version} /home/isucon/local/golang `uname -s | tr [A-Z] [a-z]` `dpkg --print-architecture`" do
   user 'isucon'
   not_if "/home/isucon/local/golang/bin/go version | grep -q 'go#{version} '"
 end
